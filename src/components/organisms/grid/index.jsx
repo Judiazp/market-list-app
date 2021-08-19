f101b4cc4b183db1cb8249bd2c693f71667795a4
Juan Carlos Diaz <judiazp2p@gmail.com>
2021-08-19 17:40:52 -0300

    Agregando estilos a las listas y persistiendo datos. - Se aplicaron estilos a las listas de productos. - Se implemento la logica para guardar las listas en el localStorage.

--- src/components/organisms/grid/index.jsx
+++ src/components/organisms/grid/index.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import { Card } from '../../molecules/card';
 import './grid.css'
 
-export const Grid = ({marketList}) => {
-    console.log(marketList);
+export const Grid = ({marketList, setMarketList}) => {
 
     return (
         <div className="grid">
-            { marketList.map(item => {
-                return <Card key={item.id} title={ item.title } id={item.id} />
-            }) }
+            { 
+                marketList.map(item => {
+                    return (
+                        <Card 
+                            key={item.id} 
+                            title={ item.title } 
+                            id={item.id} 
+                            products={ item.list }
+                            setMarketList={ setMarketList }
+                        />
+                    )
+                }) 
+            }
         </div>
     )
 }
